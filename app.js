116dbce0c37e39987f8ccd450a0924fdc9210125
Richard Turner <richardturnerdeveloper@gmail.com>
2017-07-31 21:17:56 -0400

    Add task-masterer page, add start and stop buttons, setup body-parser.

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
@@ -13,6 +16,19 @@ app.get("/home", (req, res) => {
 
 app.get("/task-masterer", (req, res) => {
   res.render('task-masterer');
+});
+
+app.post("/task-masterer", (req, res) => {
+  console.log('Redirecting!');
+  if (req.body.startTsk){
+    console.log('Starting task!');
+    res.redirect("/task-masterer");
+  } else if (req.body.stopTsk){
+    console.log('Stopping test!');
+    res.redirect("/task-masterer");
+  } else {
+    res.redirect("lost");
+  }
 });
 
 app.listen(3000, () => {
