73a4bb345d04d6140d06bb09dc7ab2198f6d3b6e
Richard Turner <richardturnerdeveloper@gmail.com>
2017-08-05 02:01:43 -0400

    Move tasks routes to external file, setup a home page view.

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const methodOverride = require('method-override');
 const hbs = require('hbs');
-const fs = require('fs');
 const bodyParser = require('body-parser');
-const {mongoose} = require('./server/db/mongoose');
-const {ObjectID} = require('mongodb');
-const {Task} = require('./server/models/task');
-const conv = require('./lib/conv');
 
 var app = express();
 
@@ -14,6 +9,7 @@ app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 hbs.registerPartials(__dirname + '/views/partials');
+
 hbs.registerHelper('viewHours', function(difference){
   difference = difference * 0.001; //convert milliseconds to seconds
   difference = difference * 0.0166667; //convert seconds to minutes
@@ -24,99 +20,11 @@ hbs.registerHelper('viewHours', function(difference){
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
 
-
 //---------ROUTES --------- //
+app.use('/tasks', require('./routes/tasks'));
 
 app.get("/", (req, res) => {
-  res.redirect("/task-masterer");
-});
-//LIST TASKS
-app.get("/task-masterer", (req, res) => {
-  Task.find().then((tasks) => {
-    res.render("task-masterer", {
-      tasks
-    });
-  }).catch((e) => {
-    res.redirect("/lost");
-  });
-});
-//ADD A NEW TASK
-app.post("/task-masterer", (req, res) => {
-  var newTask = new Task({
-    title: req.body.titleTsk
-  });
-  newTask.save().then((task) => {
-    res.redirect("/task-masterer");
-  }).catch((e) => {
-    console.log(e);
-    res.redirect("/lost");
-  });
-});
-//INDIVIDUAL TASK PAGE
-app.get("/task-masterer/:id", (req, res) => {
-  var id = req.params.id;
-  Task.findById(id).then((task) => {
-      res.render("task",{
-        task
-      });
-  }).catch((e) => {
-    console.log(e);
-    res.redirect("/lost");
-  });
-});
-
-//PATCH ROUTES
-
-app.put("/task-masterer/:id", (req, res) => {
-  var id = req.params.id;
-  if (!ObjectID.isValid(id)){
-    return res.status(400).send('Object ID not valid!');
-  }
-  // IF START BUTTON WAS CLICKED
-  if (req.body.startTsk){
-    Task.findByIdAndUpdate(id, {$set: {startTime: conv.taskCount()}}).then((task) => {
-      res.redirect('/task-masterer/' + id);
-    }).catch((e) => {
-      res.send(e);
-    });
-  }
-  // IF STOP WAS CLICKED
-  else if (req.body.stopTsk){
-    Task.findById(id).then((task) => {
-      //PREPARE DATA TO UPDATE TASK
-      var data = {
-        start: task.startTime,
-        time: task.time,
-        stop: conv.taskCount()
-      }
-      data.difference = data.stop - data.start;
-      return data;
-    }).then((data) => {
-      // USE DATA TO UPDATE TOTAL TIME
-      var newTime = data.time + data.difference;
-      Task.findByIdAndUpdate(id, {$set: {time: newTime}}).then((task) => {
-        res.redirect('/task-masterer/' + id);
-      }).catch((e) => {
-        res.redirect('lost');
-      });
-    }).catch((e) => {
-      res.redirect('lost');
-    });
-  }
-});
-
-//DELETE ROUTE
-app.delete("/task-masterer/:id", (req, res) => {
-  var id = req.params.id;
-  if (!ObjectID.isValid(id)){
-    return res.status(400).send('Object ID not valid!');
-  }
-  Task.findByIdAndRemove(id).then((task) => {
-    console.log(`${task} removed!`);
-    res.redirect("/task-masterer");
-  }).catch((e) => {
-    res.redirect("/lost");
-  });
+  res.render('home');
 });
 
 app.get("/lost", (req, res) => {
---
+++ routes/tasks.js
@@ -0,0 +1,101 @@
+var express = require('express')
+, router = express.Router();
+
+var {Task} = require('./../server/models/task');
+const {mongoose} = require('./../server/db/mongoose');
+var {ObjectID} = require('mongodb');
+const conv = require('./../lib/conv');
+
+//LIST TASKS
+router.get("/", (req, res) => {
+  Task.find().then((tasks) => {
+    res.render("tasks", {
+      tasks
+    });
+  }).catch((e) => {
+    res.redirect("/lost");
+  });
+});
+//ADD A NEW TASK
+router.post("/", (req, res) => {
+  var newTask = new Task({
+    title: req.body.titleTsk
+  });
+  newTask.save().then((task) => {
+    res.redirect("/tasks");
+  }).catch((e) => {
+    console.log(e);
+    res.redirect("/lost");
+  });
+});
+//INDIVIDUAL TASK PAGE
+router.get("/:id", (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)){
+    return res.status(400).send('Object ID not valid!');
+  }
+  Task.findById(id).then((task) => {
+      res.render("task",{
+        task
+      });
+  }).catch((e) => {
+    console.log(e);
+    res.redirect("/lost");
+  });
+});
+
+//PATCH ROUTES
+
+router.put("/:id", (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)){
+    return res.status(400).send('Object ID not valid!');
+  }
+  // IF START BUTTON WAS CLICKED
+  if (req.body.startTsk){
+    Task.findByIdAndUpdate(id, {$set: {startTime: conv.taskCount()}}).then((task) => {
+      res.redirect('/tasks/' + id);
+    }).catch((e) => {
+      res.send(e);
+    });
+  }
+  // IF STOP WAS CLICKED
+  else if (req.body.stopTsk){
+    Task.findById(id).then((task) => {
+      //PREPARE DATA TO UPDATE TASK
+      var data = {
+        start: task.startTime,
+        time: task.time,
+        stop: conv.taskCount()
+      }
+      data.difference = data.stop - data.start;
+      return data;
+    }).then((data) => {
+      // USE DATA TO UPDATE TOTAL TIME
+      var newTime = data.time + data.difference;
+      Task.findByIdAndUpdate(id, {$set: {time: newTime}}).then((task) => {
+        res.redirect('/tasks/' + id);
+      }).catch((e) => {
+        res.redirect('lost');
+      });
+    }).catch((e) => {
+      res.redirect('lost');
+    });
+  }
+});
+
+//DELETE ROUTE
+router.delete("/:id", (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)){
+    return res.status(400).send('Object ID not valid!');
+  }
+  Task.findByIdAndRemove(id).then((task) => {
+    console.log(`${task} removed!`);
+    res.redirect("/tasks");
+  }).catch((e) => {
+    res.redirect("/lost");
+  });
+});
+
+module.exports = router;
