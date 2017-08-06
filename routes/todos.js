3253ccd47e8925df37da0c21c0c91801ca615408
Richard Turner <richardturnerdeveloper@gmail.com>
2017-08-06 04:00:10 -0400

    Style todo index page

--- routes/todos.js
+++ routes/todos.js
@@ -5,9 +5,25 @@ var {Todo} = require('./../server/models/todo');
 const {mongoose} = require('./../server/db/mongoose');
 var {ObjectID} = require('mongodb');
 const conv = require('./../lib/conv');
-
+//VIEW TODOS
 router.get("/", (req, res) => {
-  res.render('todos');
+  Todo.find().then((todos) => {
+      res.render('todos', {todos});
+  }).catch((e) => {
+    res.redirect('lost');
+  })
+
+});
+//ADD A todo
+router.post("/", (req, res) => {
+  if (req.body.titleTodo){
+    var newTodo = new Todo({
+      title: req.body.titleTodo.toUpperCase()
+    });
+    newTodo.save().then((todo) => {
+      res.redirect("/todos");
+    });
+  }
 });
 
 module.exports = router;
