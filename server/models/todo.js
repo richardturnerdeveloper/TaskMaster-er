f0e2ca2cd509cba88544e27fcbb0f1064726bf51
Richard Turner <richardturnerdeveloper@gmail.com>
2017-08-06 03:25:23 -0400

    Update Todo model

---
+++ server/models/todo.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo', {
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    maxlength: 40,
+    minlength: 1
+  }, completed: {
+    type: Boolean,
+    default: false,
+    required: true
+  }, dateStarted: {
+    type: String,
+    default: new Date().toDateString(),
+    required: true
+  }
+});
+
+module.exports = {Todo};
