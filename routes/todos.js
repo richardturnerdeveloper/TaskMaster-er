c9d309f31461b7d5e7ae07eef9b380fb05d338cd
Richard Turner <richardturnerdeveloper@gmail.com>
2017-08-07 01:30:57 -0400

    Add error handling for todo routes, update order of displayed todos to show newest first

--- routes/todos.js
+++ routes/todos.js
@@ -8,10 +8,13 @@ const conv = require('./../lib/conv');
 
 //VIEW TODOS
 router.get("/", (req, res) => {
-  Todo.find().then((todos) => {
-      res.render('todos', {todos});
+  Todo.find().sort({_id: -1}).then((todos) => {
+      res.status(200).render('todos', {todos});
   }).catch((e) => {
-    res.redirect('lost');
+    res.status(404).render('lost', {
+      errMessage: 'could not find any TODO items',
+      url: '/todos'
+    });
   })
 
 });
@@ -23,6 +26,11 @@ router.post("/", (req, res) => {
     });
     newTodo.save().then((todo) => {
       res.redirect("/todos");
+    }).catch((e) => {
+      res.status(404).render('lost', {
+        errMessage: 'could not add that Todo! Please try again.',
+        url: '/todos'
+      });
     });
   }
 });
@@ -30,24 +38,36 @@ router.post("/", (req, res) => {
 router.put("/:id", (req, res) => {
   var id = req.params.id;
   if (!ObjectID.isValid(id)){
-    return res.status(400).send('Object ID not valid!');
+    res.status(400).render('lost', {
+      errMessage: 'that is not a valid ID!',
+      url: '/todos'
+    });
   }
   Todo.findByIdAndUpdate(id, {$set: {completed: true, color: 'green'}}).then((todo) => {
     res.redirect("/todos");
   }).catch((e) => {
-    res.redirect("/lost");
+    res.status(404).render('lost', {
+      errMessage: 'that TODO could not be updated at this time! Woopsie!',
+      url: '/todos'
+    });
   });
 });
 //DELETE A todo
 router.delete('/:id', (req, res) => {
   var id = req.params.id;
   if (!ObjectID.isValid(id)){
-    return res.status(400).send('Object ID not valid!');
+    res.status(400).render('lost', {
+      errMessage: 'that is not a valid ID!',
+      url: '/todos'
+    });
   }
   Todo.findByIdAndRemove(id).then((todo) => {
     res.redirect("/todos");
   }).catch((e) => {
-    res.redirect("lost");
+    res.status(400).render('lost', {
+      errMessage: 'could not remove that todo at this time!',
+      url: '/todos'
+    });
   });
 
 });
